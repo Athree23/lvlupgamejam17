53209ba81f2262b6080cf2bf54257fcb037ea12b
olivier <olivier.wenger@edu.hefr.ch>
2017-02-17 09:44:30 +0100

    priest movement fixed

--- priest/Assets/Scripts/PriestMovement.cs
+++ priest/Assets/Scripts/PriestMovement.cs
@@ -11,12 +11,14 @@ public class PriestMovement : MonoBehaviour {
     //distance to player
     private float range;
     //speed to move
-    public float Speed;
+    public float speed;
+    public float rotationSpeed;
     
 	// Use this for initialization
 	void Start () {
         //Looking for object with tag "player"
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        
         //priest = GameObject.FindGameObjectWithTag("priest").transform;
         
 
@@ -24,13 +26,35 @@ public class PriestMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //transform.LookAt(player);
         range = Vector2.Distance(transform.position, player.position);
-        
-        if (range <= 10f)
+        if(range <= 10f)
         {
-            transform.Translate(Vector2.MoveTowards(transform.position, player.position, range) * Speed * Time.deltaTime);
+            Vector2 targetDir = new Vector2(player.position.x, transform.position.y).normalized;
+            transform.position = Vector2.MoveTowards(transform.position, targetDir, speed * Time.deltaTime);
+            //transform.position += transform.forward * speed*Time.deltaTime;
         }
 
-		
-	}
+
+        /* player = GameObject.FindGameObjectWithTag("Player").transform;
+
+         range = Vector2.Distance(transform.position, player.position);
+         Vector3 targetHeading = player.position - transform.position;
+         Vector3 targetDir = targetHeading.normalized;
+
+         //rotate to look at the player
+         transform.rotation = Quaternion.LookRotation(targetDir), rotationSpeed*Time.deltaTime;
+         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+         transform.position += transform.forward * Speed * Time.deltaTime;       
+
+         if (range <= 10f)
+         {
+             transform.Translate(Vector2.MoveTowards(transform.position, player.position, range).normalized * Speed * Time.deltaTime);
+         }
+         */
+
+
+    }
 }
