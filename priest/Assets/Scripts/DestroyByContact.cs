8cdca411d5aa5d2f4f0a601015cabac587500254
Simon Lievre <simon.lievre@edu.hefr.ch>
2017-02-17 21:24:08 +0100

    simon

--- priest/Assets/Scripts/DestroyByContact.cs
+++ priest/Assets/Scripts/DestroyByContact.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -16,4 +17,24 @@ public class DestroyByContact : MonoBehaviour
         }
         Destroy(gameObject);
     }
+=======
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public float damage = 10f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Player") return;
+
+        if (other.tag == "Enemy")
+        {
+            PriestHealth enemy = (PriestHealth)other.gameObject.GetComponent<PriestHealth>();
+            if (enemy != null) enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+>>>>>>> Stashed changes
 }
--- priest/Assets/Scripts/PriestAttack.cs
+++ priest/Assets/Scripts/PriestAttack.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -70,3 +71,77 @@ public class PriestAttack : MonoBehaviour {
 
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriestAttack : MonoBehaviour {
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    PriestHealth priestHealth;
+    bool playerInRange;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        priestHealth = GetComponent<PriestHealth>();
+        anim = player.GetComponent<Animator>();
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if the entering collider is the player
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        //if the exiting collider is the player
+        if(collision.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+
+        //if it's time to attack and player in range
+        if(timer >= timeBetweenAttacks && playerInRange && priestHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+
+        //if playerDead set the anim
+        if(playerHealth.currentHealth <= 0)
+        {
+            //anim.SetTrigger("PlayerDead");
+        }
+		
+	}
+    void Attack()
+    {
+        //reset timer
+        timer = 0f;
+
+        //if player has health to lose
+        if(playerHealth.currentHealth > 0)
+        {
+            //damage the player
+            playerHealth.TakeDamage(attackDamage);
+        }
+
+    }
+}
+>>>>>>> Stashed changes
