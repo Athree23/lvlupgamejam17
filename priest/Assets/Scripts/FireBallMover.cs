7d17fcce97f6d21000f2d38094993dd95f28d9e1
Adrian Enderlin <adrian.enderlin@bluewin.ch>
2017-02-17 10:32:59 +0100

    asf

---
+++ priest/Assets/Scripts/FireBallMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallMover : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        UnityStandardAssets._2D.PlayerController player = (UnityStandardAssets._2D.PlayerController)GameObject.FindObjectOfType(typeof(UnityStandardAssets._2D.PlayerController));
+        Vector2 dir = new Vector2(player.getDir(), 0);
+        rb.AddForce(dir*speed);
+    }
+}
--- priest/Assets/Scripts/PriestMovement.cs
+++ priest/Assets/Scripts/PriestMovement.cs
@@ -4,40 +4,61 @@ using UnityEngine;
 using UnityEngine.AI;
 
 
-public class PriestMovement : MonoBehaviour {
+public class PriestMovement : MonoBehaviour
+{
     //goal of the priest
     Transform player;
     Transform priest;
     //distance to player
     private float range;
     //speed to move
-    public float Speed;
-    
-	// Use this for initialization
-	void Start () {
+    public float speed;
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
         //Looking for object with tag "player"
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
         //priest = GameObject.FindGameObjectWithTag("priest").transform;
-        
+
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.LookAt(player);
         range = Vector2.Distance(transform.position, player.position);
-        
         if (range <= 10f)
         {
-<<<<<<< HEAD
 
             Vector2 targetDir = new Vector2(player.position.x, transform.position.x).normalized;
             transform.position = Vector2.MoveTowards(transform.position, targetDir, speed * Time.deltaTime);
             //transform.position += transform.forward * speed*Time.deltaTime;
-=======
-            transform.Translate(Vector2.MoveTowards(transform.position, player.position, range) * Speed * Time.deltaTime);
->>>>>>> 31bee79eac3e929cff8ae8a33e95d35865c68c28
         }
 
-		
-	}
+
+        /* player = GameObject.FindGameObjectWithTag("Player").transform;
+
+         range = Vector2.Distance(transform.position, player.position);
+         Vector3 targetHeading = player.position - transform.position;
+         Vector3 targetDir = targetHeading.normalized;
+
+         //rotate to look at the player
+         transform.rotation = Quaternion.LookRotation(targetDir), rotationSpeed*Time.deltaTime;
+         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+
+         transform.position += transform.forward * Speed * Time.deltaTime;       
+
+         if (range <= 10f)
+         {
+             transform.Translate(Vector2.MoveTowards(transform.position, player.position, range).normalized * Speed * Time.deltaTime);
+         }
+         */
+
+
+    }
 }
