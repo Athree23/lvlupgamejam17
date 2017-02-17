4ec86152be7ad0169242232357c319e3e367f36e
olivier <olivier.wenger@edu.hefr.ch>
2017-02-17 01:48:12 +0100

    priest movement (need to make it move only on x-axis)

--- priest/Assets/Scripts/PriestMovement.cs
+++ priest/Assets/Scripts/PriestMovement.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class PriestMovement : MonoBehaviour {
     //goal of the priest
     Transform player;
-    //reference to pathfinding
-    NavMeshAgent nav;
+    Transform priest;
+    //distance to player
+    private float range;
+    //speed to move
+    public float Speed;
+    
 	// Use this for initialization
 	void Start () {
         //Looking for object with tag "player"
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        //priest = GameObject.FindGameObjectWithTag("priest").transform;
+        
 
-        nav = GetComponent<NavMeshAgent>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        nav.SetDestination(player.position);
+        range = Vector2.Distance(transform.position, player.position);
+        
+        if (range <= 10f)
+        {
+            transform.Translate(Vector2.MoveTowards(transform.position, player.position, range) * Speed * Time.deltaTime);
+        }
 
 		
 	}
