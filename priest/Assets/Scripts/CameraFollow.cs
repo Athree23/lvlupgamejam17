df0f453068afa19e8eaa6777aa265039b7d3f78c
olivier <olivier.wenger@edu.hefr.ch>
2017-02-23 19:19:46 +0100

    added crouch key to lower the camera

--- priest/Assets/Scripts/CameraFollow.cs
+++ priest/Assets/Scripts/CameraFollow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 
 public class CameraFollow : MonoBehaviour
@@ -11,13 +12,15 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     [SerializeField]
     private Camera camera;
+    private bool m_crouch;
 
     // Update is called once per frame
     void Update()
     {
         if (target)
         {
-            
+            m_crouch = CrossPlatformInputManager.GetButton("Crouch");
+
             Vector3 point = camera.WorldToViewportPoint(target.position);
             float z = 10f;
             PlayerMovement plyctrl = (PlayerMovement)target.gameObject.GetComponent<PlayerMovement>();
@@ -25,7 +28,17 @@ public class CameraFollow : MonoBehaviour
             {
                 z = 9.5f;
             }
-            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.3f, z)); //(new Vector3(0.5, 0.5, point.z));
+
+            Vector3 delta;
+            if(m_crouch)
+            {
+                delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.9f, z)); //(new Vector3(0.5, 0.5, point.z));
+            }
+            else
+            {
+                delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.3f, z)); //(new Vector3(0.5, 0.5, point.z));
+            }
+            //delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.3f, z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
